84d2c252f0f15b65f9da9996b8591d6d6aa4ba88
Michael <GrantM1@users.noreply.github.com>
2020-03-27 16:07:38 +0100

    working with HTML and CSS learned some new trick like  clip-path:

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ $(document).ready(function () {
   $('.nav-links').slideToggle(1000);
  })
 })
+
+$(window).scroll(function () {
+ let scroll = $(window).scrollTop();
+ if (scroll >= 0) {
+  $('#nav').addClass("new-nav");
+ } else {
+  $('#nav').removeClass("new-nav");
+ }
+})
+
+
+$('nav a').click(function (link) {
+ link.preventDefault();
+ let traget = $(this).attr("href");
+ $('html body').animate({
+  scrollTop: $(target).offset().top
+ }, "slow");
+})
