a387f7d6b53852606f98b159c456c3259c1542d7
Michael <GrantM1@users.noreply.github.com>
2020-03-30 20:27:27 +0200

    implementing owl css,bug fixing

--- script.js
+++ script.js
@@ -1,52 +1,73 @@
 $(document).ready(function () {
- $("#toggle-btn").click(function () {
-  $('.nav-links').slideToggle(1000);
- })
+    $("#toggle-btn").click(function () {
+        $('.nav-links').slideToggle(1000);
+    })
 })
 
 $(window).scroll(function () {
- let scroll = $(window).scrollTop();
- if (scroll >= 0) {
-  $('#nav').addClass("new-nav");
- } else {
-  $('#nav').removeClass("new-nav");
- }
+    let scroll = $(window).scrollTop();
+    if (scroll >= 0) {
+        $('#nav').addClass("new-nav");
+    } else {
+        $('#nav').removeClass("new-nav");
+    }
 })
 
 
 $('nav a').click(function (link) {
- link.preventDefault();
- let target = $(this).attr("href");
- $('html, body').animate({
-  scrollTop: $(target).offset().top
- }, "slow");
+    link.preventDefault();
+    let target = $(this).attr("href");
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, "slow");
 })
 
 $(function () {
- $("#accordion").accordion({
-  animate: 500
- });
+    $("#accordion").accordion({
+        animate: 500
+    });
 });
 
 // progress bars
 
 $(function () {
- $("#p-bar-1").progressbar({
-  value: 37
- });
+    $("#p-bar-1").progressbar({
+        value: 37
+    });
 });
 $(function () {
- $("#p-bar-2").progressbar({
-  value: 70
- });
+    $("#p-bar-2").progressbar({
+        value: 70
+    });
 });
 $(function () {
- $("#p-bar-3").progressbar({
-  value: 60
- });
+    $("#p-bar-3").progressbar({
+        value: 60
+    });
 });
 $(function () {
- $("#p-bar-4").progressbar({
-  value: 80
- });
+    $("#p-bar-4").progressbar({
+        value: 80
+    });
 });
+
+
+// owl carousel 
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    smartSpeed: 2000,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
